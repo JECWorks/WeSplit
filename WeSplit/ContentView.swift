eef44ad5a97ff32a4e4073f2bc97f88c1ff66d6d
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-07-30 22:21:45 -0400

    Begin WeSplit app NavigationStack

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,35 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     // @State signals the var is made here and only used here.
-    @State var tapCount = 0
-    @State private var name = ""
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
+    // $ before the variable makes it two-way binding
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     
     var body: some View {
-        VStack {
-            NavigationStack {
-                Form {
-                    //                TextField("Enter your name", text: $name)
-                    //                Button("Tap Count: \(tapCount)") {
-                    //                    tapCount += 1
-                    //                }
-                    //                Text("Your name is \(name)")
-                    //                ForEach(0..<100) { number in
-                    //                    Text("Row \(number)")
-                    //                }
-                    // This does the same thing as above:
-//                    ForEach(0..<4) {
-//                        Text("Row \($0)")
-//                    }
-                    Picker("Select your student", selection: $selectedStudent) {
-                        ForEach(students, id: \.self) {
-                            Text($0)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<10) {
+                            Text("\($0) people")
                         }
                     }
+//                    .pickerStyle(.navigationLink)
                 }
-                .navigationTitle("Select a Student")
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
+            .navigationTitle("WeSplit")
         }
         
     }
