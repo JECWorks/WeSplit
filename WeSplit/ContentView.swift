f526fc31f4bdadbc6afbd12d98b399ca4851bef1
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-07-30 21:41:26 -0400

    Textfield, Button, and Picker

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,18 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    // @State signals the var is made here and only used here.
+    @State var tapCount = 0
+    @State private var name = ""
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
-        NavigationStack {
-            Form {
-                Section {
-                    Text("Hello, world!")
+        VStack {
+            NavigationStack {
+                Form {
+                    //                TextField("Enter your name", text: $name)
+                    //                Button("Tap Count: \(tapCount)") {
+                    //                    tapCount += 1
+                    //                }
+                    //                Text("Your name is \(name)")
+                    //                ForEach(0..<100) { number in
+                    //                    Text("Row \(number)")
+                    //                }
+                    // This does the same thing as above:
+//                    ForEach(0..<4) {
+//                        Text("Row \($0)")
+//                    }
+                    Picker("Select your student", selection: $selectedStudent) {
+                        ForEach(students, id: \.self) {
+                            Text($0)
+                        }
+                    }
                 }
-                Text("Hello, world!")
-                Text("Hello, world!")
+                .navigationTitle("Select a Student")
             }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
         }
+        
     }
 }
 #Preview {
